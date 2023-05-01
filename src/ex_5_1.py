639904258cb1763f1d270295df7847d767971471
pavithracbu <parockia@cbu.edu>
2023-05-01 07:57:47 +0530

    completed

--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -10,9 +10,9 @@ except ImportError:
 
 def main(infile):
     """Call line_count with the infile argument."""
-    #line_count(infile)
-    count = line_count(infile)
-    print(f"The file '{infile}' has {count} lines")
+    line_count(infile)
+    #count = line_count(infile)
+    #print(f"The file '{infile}' has {count} lines")
 
 
 if __name__ == "__main__":
@@ -22,9 +22,9 @@ if __name__ == "__main__":
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    parser = argparse.ArgumentParser(description='Count number of lines in a file')
-    parser.add_argument('infile', help='enter input file name')
+    parser = argparse.ArgumentParser(description='This program prints the number of lines in infile.')
+    parser.add_argument('-infile', help='enter input file name')
     args = parser.parse_args()
 
-
-    main(args.infile)
+    if args.infile:
+        main(args.infile)
